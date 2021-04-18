b7b9bd35b36366ca37c5c4330de1e5f9be6145fc
Jonas Hendel <70843626+JonasHendel@users.noreply.github.com>
2021-04-18 10:56:45 +0200

    Delete next.config.js

--- client/next.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  env: {
-    API_URL_MAILCHIMP: 'https://gmail.us1.list-manage.com/subscribe/post?u=626959619ccc4341d70b0b034&amp;id=e2787d42b4',
-    USER_EMAILJS: 'user_FumSeNhN6BKsgunkFl7DJ',
-    MONGO_URI: 'mongodb+srv://admin:admin@francescodb.c8uwh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
-    BASE_URL: 'http://localhost:3000',
-  },
-}
