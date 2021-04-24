3a607b135cfeb1667986a097c8bbd3c8347e9131
JonasHendel <70843626+JonasHendel@users.noreply.github.com>
2021-04-24 19:18:49 +0200

    change baseurl

--- client/next.config.js
+++ client/next.config.js
@@ -1,12 +1,12 @@
 module.exports = {
   env: {
-    USER_EMAILJS: process.env.USER_EMAILJS,
-    MONGO_URI: process.env.MONGO_URI,
-    BASE_URL: process.env.BASE_URL,
-    MAILCHIMP_API_KEY: process.env.MAILCHIMP_API_KEY,
-    MAILCHIMP_API_SERVER: process.env,
-    MAILCHIMP_AUDIENCE_ID: process.env.MAILCHIMP_API_SERVER,
-    ACCESS_TOKEN_SECRET: process.env.ACCESS_TOKEN_SECRET,
-    REFRESH_TOKEN_SECRET: process.env.REFRESH_TOKEN_SECRET
+    USER_EMAILJS: 'user_FumSeNhN6BKsgunkFl7DJ',
+    MONGO_URI: 'mongodb+srv://admin:admin@francescodb.c8uwh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+    BASE_URL: 'https://fs-dev.vercel.app/',
+    MAILCHIMP_API_KEY: '920b2f6b34b0124c6c2b7e75369d2b17-us1',
+    MAILCHIMP_API_SERVER: 'us1',
+    MAILCHIMP_AUDIENCE_ID: 'e2787d42b4',
+    ACCESS_TOKEN_SECRET: 'q+?pAzCo+bB1F8%09Cf@G6_I!8-B#Ww%B!Wg=AzKVNk+|!1O?9',
+    REFRESH_TOKEN_SECRET: 'Nxqc|nBf#%S9KyFo2Y8l|btEEpb@4fXtiXZ%Zl%htP8iS7NL8ZJI@9+pO$2%WzAyFgURSmbG*6B0HNkD'
   },
 }
