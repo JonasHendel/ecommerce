c95345ef9c69c38916860ac03e4fa86cac78eaa3
JonasHendel <70843626+JonasHendel@users.noreply.github.com>
2021-04-24 23:19:16 +0200

    added google analytics

--- client/components/Layout.js
+++ client/components/Layout.js
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { useContext } from 'react'
+// import LogRocket from 'logrocket';
+
+import { DataContext } from '../store/GlobalState'
 
 import NavBar from './NavBar'
 import Notify from './Notify'
 
 function Layout({children}) {
+
+  const {state} = useContext(DataContext)
+
+
+  // LogRocket.init('xfw64p/francesco-site');
+
+  // if(state.auth.user) {
+  //   LogRocket.identify('user', {
+  //     name: state.auth.user.name,
+  //     email: state.auth.user.email,
+  //   });
+  // }
+
+
+  
   return (
     <div>
         <NavBar/>
--- client/pages/_app.js
+++ client/pages/_app.js
@@ -4,7 +4,18 @@ import Layout from '../components/Layout'
 
 import { DataProvider } from '../store/GlobalState'  
 
+import  ReactGa from 'react-ga'
+import { useEffect } from 'react'
+
+
 function MyApp({ Component, pageProps }) {
+  
+  useEffect(() => {
+    ReactGa.initialize('G-QRSCQJW7N1')
+
+    ReactGa.pageview('/')
+  }, [])
+
   return (
     <>
       <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
