fa198edfb5c7dc55a6cb5ef117ddb9f173c630f5
JonasHendel <70843626+JonasHendel@users.noreply.github.com>
2021-05-07 22:13:54 +0200

    empty cart on successfull payment

--- client/pages/success.js
+++ client/pages/success.js
@@ -1,7 +1,19 @@
 import Link from 'next/link';
 import { ArrowLeft } from 'phosphor-react';
+import { useEffect } from 'react';
+import {useContext} from 'react'
+import { DataContext } from '../store/GlobalState'
 
 const Success = () => {
+  const {state, dispatch}  = useContext(DataContext)
+
+  useEffect(()=>{
+    localStorage.removeItem('__next__cart01__solimeo')
+    dispatch({ type: 'ADD_CART', payload: []})
+  }, [])
+
+
+
 	return (
 		<div className='flex flex-col h-screen items-center'>
 			<div className='flex mt-60 flex-col items-center p-12 py-16 bg-gray-100 rounded-md shadow-xl'>
