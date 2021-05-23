ff41e1a71418a3ccad2f6c9650eb1b28f94a40b1
JonasHendel <70843626+JonasHendel@users.noreply.github.com>
2021-05-23 16:52:13 +0200

    added semicolons CustomForm

--- client/components/Newsletter/CustomForms.js
+++ client/components/Newsletter/CustomForms.js
@@ -1,19 +1,18 @@
-import {DataContext} from '../../store/GlobalState'
-import {useContext, useState} from 'react'
+import { DataContext } from '../../store/GlobalState';
+import { useContext, useState } from 'react';
 import { useEffect } from 'react/cjs/react.development';
 
 const FooterForm = ({ status, message, onValidated, setActive }) => {
+	const [userEmail, setUserEmail] = useState('');
 
-  const [userEmail, setUserEmail] = useState('')
-  
-  const {state} = useContext(DataContext)
-  const {auth} = state
-  
-  useEffect(()=>{
-    if(auth.user){
-      setUserEmail(auth.user.email)
-    }
-  },[auth])
+	const { state } = useContext(DataContext);
+	const { auth } = state;
+
+	useEffect(() => {
+		if (auth.user) {
+			setUserEmail(auth.user.email);
+		}
+	}, [auth]);
 
 	let email;
 	const submit = (e) => {
@@ -30,22 +29,30 @@ const FooterForm = ({ status, message, onValidated, setActive }) => {
 
 	return (
 		<>
-					<form className="flex flex-col justify-center h-full" onSubmit={submit}>
-            <div>
-              <h1 className="text-lg w-60">Subscribe to my newsletter!</h1>
-            </div>
-						<input
-							className='text-black rounded-lg p-2 w-60 focus:outline-none'
-							ref={(node) => (email = node)}
-							type='email'
-							placeholder='Your email'
-              defaultValue={userEmail}
-						/>
-						<button className="border-2 border-gray-50 p-2 my-2 rounded-lg w-60 focus:outline-none active:bg-white" type='submit'>Subscribe</button>
-					</form>
-					<br />
-					<div>
-						{/* {status === 'error' && (
+			<form
+				className='flex flex-col justify-center h-full'
+				onSubmit={submit}>
+				<div>
+					<h1 className='text-lg w-60'>
+						Subscribe to my newsletter!
+					</h1>
+				</div>
+				<input
+					className='text-black rounded-lg p-2 w-60 focus:outline-none'
+					ref={(node) => (email = node)}
+					type='email'
+					placeholder='Your email'
+					defaultValue={userEmail}
+				/>
+				<button
+					className='border-2 border-gray-50 p-2 my-2 rounded-lg w-60 focus:outline-none active:bg-white'
+					type='submit'>
+					Subscribe
+				</button>
+			</form>
+			<br />
+			<div>
+				{/* {status === 'error' && (
 							<div>
 								{message.includes('already subscribed') ? (
 									<p>Already subscribed!</p>
@@ -54,9 +61,9 @@ const FooterForm = ({ status, message, onValidated, setActive }) => {
 								)}
 							</div>
 						)} */}
-					</div>
+			</div>
 		</>
 	);
 };
 
-export default FooterForm
+export default FooterForm;
